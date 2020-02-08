419d336531e89a2b7cd4e11d8d692683e09745d0
Karolina Florek <emertha@gmail.com>
2020-02-08 21:00:22 +0100

    klasa

---
+++ KarolinaTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using marekKarolinaProjekt;
+
+
+namespace KarolinaTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Specyfiction_Empty_All_String_Throws_Exception()
+        {
+            Assert.Throws<ArgumentException>(
+            () =>
+            {
+                new Specyfication(
+                    "", "", "",""
+                  );
+            }
+            );
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+        }
+    }
+}
---
+++ marekKarolinaProjekt/Specyfication.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marekKarolinaProjekt
+{
+    public class Specyfication
+    {
+
+        private readonly String ram;
+        private readonly String graphic;
+        private readonly String processor;
+        private readonly String screen;
+
+        public Specyfication(String ram, String graphic, String processor, String screen)
+        {
+            this.ram = ram;
+            this.graphic = graphic;
+            this.processor = processor;
+            this.screen = screen;
+        }
+
+        public String Ram { get => ram; }
+
+        public String Graphic { get => graphic; }
+
+        public String Processor { get => processor; }
+
+        public String Screen { get => screen; }
+
+    }
+}
